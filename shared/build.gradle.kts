ad0cfbf2c631897a7ad87df691b2486285a2971c
LoopyLuci <lucidluci@duck.com>
2026-08-15 20:02:22 -0400

    Android scaffold fixes: modern permissions, layout, and shared module build file

---
+++ shared/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.portableprint.shared"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 26
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
